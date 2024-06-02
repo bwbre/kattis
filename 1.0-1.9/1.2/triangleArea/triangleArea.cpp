d97a21bbe0f710e6386d7d412c47fd95caba56d4
Brendan Womer <bwbrendan@gmail.com>
2024-06-02 14:15:16 -0400

    -created TriangleArea (1.2) folder and cpp file -updated commentTemplate.txt

---
+++ 1.0-1.9/1.2/triangleArea/triangleArea.cpp
@@ -0,0 +1,20 @@
+/*
+Brendan Womer
+Kattis Problem: Triangle Area
+Difficulty: 1.2
+
+---------------
+-Explanation: Per-Magnus managed to solve his addition homework thanks to you, but now he is unfortunately posed with an even greater problem. 
+  He has been given a triangular cake by his mother, and wonders what the area of it is. It turns out Per-Magnus forgot the formula for computing it!
+  Per-Magnus managed to measure the base and height of the triangle with a ruler. Given this, compute the area of the triangular cake.
+
+-Input:
+
+
+-Output:
+
+---------------
+
+Notes: 
+
+*/
